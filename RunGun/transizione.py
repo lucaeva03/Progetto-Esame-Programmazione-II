44b6acd14769cbabf8b9ac471bd19114da9eeda5
Luca <154439810+lucaeva03@users.noreply.github.com>
2024-12-17 18:48:58 +0100

    transizione.py

--- RunGun/transizione.py
+++ RunGun/transizione.py
@@ -4,8 +4,7 @@ from img import *
 # CLASSE PER TRANSIZIONE    
 class Transizione():
 
-    def __init__(self, direction, colour, speed):
-        self.direction = direction 
+    def __init__(self, colour, speed):
         self.colour = colour 
         self.speed = speed 
         self.fade_counter = 0 
@@ -14,14 +13,10 @@ class Transizione():
     def fade(self):
         fade_complete = False 
         self.fade_counter += self.speed 
-        # FADE IN ORIZZONTALE VERSO DESTRA
-        if self.direction == 1: 
-            pygame.draw.rect(screen, self.colour, (0 - self.fade_counter, 0, SCREEN_WIDTH // 2, SCREEN_HEIGHT))
-            pygame.draw.rect(screen, self.colour, (SCREEN_WIDTH // 2 + self.fade_counter, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
-        # FADE IN VERTICALE VERSO BASSO
-        if self.direction == 2: 
-            pygame.draw.rect(screen, self.colour, (0, 0, SCREEN_WIDTH, 0 - self.fade_counter))
+        # FADE IN ORIZZONTALE DA CENTRO VERSO DESTRA E SINISTRA
+        pygame.draw.rect(screen, self.colour, (0, 0, SCREEN_WIDTH // 2  - self.fade_counter, SCREEN_HEIGHT)) # (schermo, colore, (x,y,larghezza,altezza))
+        pygame.draw.rect(screen, self.colour, (SCREEN_WIDTH // 2 + self.fade_counter, 0, SCREEN_WIDTH , SCREEN_HEIGHT))
         # CONTROLLO VALORE CONTATORE (400 per completare animazione) e RITORNO (animazione completata)
         if self.fade_counter >= 400: 
             fade_complete = True 
-        return fade_complete 
+        return fade_complete
